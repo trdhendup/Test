21d41ca3ca01a4f0f8e8dca4f4508d42511e52e3
KWESIEMPRAH <146785221+KWESIEMPRAH@users.noreply.github.com>
2023-10-12 20:19:29 -0400

    Update my_test.sql
    Brief bio description

--- my_test.sql
+++ my_test.sql
@@ -1,1 +1,2 @@
-
+Kwesi is a Ghanaian first year MBA student with concentrations in data analytics and strategy and innovation. 
+He had his undergraduate degree in BSc Accounting and information systems.  
